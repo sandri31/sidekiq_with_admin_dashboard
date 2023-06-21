e74834ea3c93ac37ab7ea9f2f75f770e5837a124
Florent S <sandri822@hotmail.fr>
2023-06-21 10:50:39 +0200

    ➕ Add a dependency Devise

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
 gem "sidekiq", "~> 7.1"
+
+gem "devise", "~> 4.9"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  devise_for :users
   authenticate :user do
     mount Sidekiq::Web => '/sidekiq'
   end
