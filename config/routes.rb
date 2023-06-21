b3ef1a59659d8ff215fefdc1c2515bafd2de4ce7
Florent S <sandri822@hotmail.fr>
2023-06-21 11:00:36 +0200

    ✨ Add Sidekiq dashboard access for admin users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
   devise_for :users
-  authenticate :user do
+  authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
   root 'pages#home'
